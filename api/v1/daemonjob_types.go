8ec495eb8579d695a8f8ac9497ac865ce9720905
i516296 <jixiang.xv@sap.com>
2021-11-24 10:05:04 +0800

    update controller logic

--- api/v1/daemonjob_types.go
+++ api/v1/daemonjob_types.go
@@ -17,6 +17,8 @@ limitations under the License.
 package v1
 
 import (
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,14 +30,53 @@ type DaemonJobSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
+	// Specifies the duration in seconds relative to the startTime that the job may be active
+	// before the system tries to terminate it; value must be positive integer
+	// +optional
+	ActiveDeadlineSeconds *int64 `json:"activeDeadlineSeconds,omitempty" protobuf:"varint,3,opt,name=activeDeadlineSeconds"`
+
+	// Specifies the number of retries before marking this job failed.
+	// Defaults to 6
+	// +optional
+	BackoffLimit *int32 `json:"backoffLimit,omitempty" protobuf:"varint,7,opt,name=backoffLimit"`
+
+	// ttlSecondsAfterFinished limits the lifetime of a Job that has finished
+	// execution (either Complete or Failed). If this field is set,
+	// ttlSecondsAfterFinished after the Job finishes, it is eligible to be
+	// automatically deleted. When the Job is being deleted, its lifecycle
+	// guarantees (e.g. finalizers) will be honored. If this field is unset,
+	// the Job won't be automatically deleted. If this field is set to zero,
+	// the Job becomes eligible to be deleted immediately after it finishes.
+	// This field is alpha-level and is only honored by servers that enable the
+	// TTLAfterFinished feature.
+	// +optional
+	TTLSecondsAfterFinished *int32 `json:"ttlSecondsAfterFinished,omitempty" protobuf:"varint,8,opt,name=ttlSecondsAfterFinished"`
+
+	// A label query over pods that should match the pod count.
+	// Normally, the system sets this field for you.
+	// More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#label-selectors
+	// +optional
+	Selector *metav1.LabelSelector `json:"selector,omitempty" protobuf:"bytes,4,opt,name=selector"`
+
+	// Describes the pod that will be created when executing a job.
+	// More info: https://kubernetes.io/docs/concepts/workloads/controllers/jobs-run-to-completion/
+	Template corev1.PodTemplateSpec `json:"template" protobuf:"bytes,6,opt,name=template"`
+
+	// manualSelector controls generation of pod labels and pod selectors.
+	// Leave `manualSelector` unset unless you are certain what you are doing.
+	// When false or unset, the system pick labels unique to this job
+	// and appends those labels to the pod template.  When true,
+	// the user is responsible for picking unique labels and specifying
+	// the selector.  Failure to pick a unique label may cause this
+	// and other jobs to not function correctly.  However, You may see
+	// `manualSelector=true` in jobs that were created with the old `extensions/v1beta1`
+	// API.
+	// More info: https://kubernetes.io/docs/concepts/workloads/controllers/jobs-run-to-completion/#specifying-your-own-pod-selector
+	// +optional
+	ManualSelector *bool `json:"manualSelector,omitempty" protobuf:"varint,5,opt,name=manualSelector"`
+
 	// Foo is an example field of DaemonJob. Edit daemonjob_types.go to remove/update
 	Foo string `json:"foo,omitempty"`
-}
-
-// DaemonJobStatus defines the observed state of DaemonJob
-type DaemonJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 //+kubebuilder:object:root=true
@@ -46,8 +87,8 @@ type DaemonJob struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   DaemonJobSpec   `json:"spec,omitempty"`
-	Status DaemonJobStatus `json:"status,omitempty"`
+	Spec   DaemonJobSpec      `json:"spec,omitempty"`
+	Status *batchv1.JobStatus `json:"status,omitempty"`
 }
 
 //+kubebuilder:object:root=true
--- controllers/daemonjob_controller.go
+++ controllers/daemonjob_controller.go
@@ -19,23 +19,29 @@ package controllers
 import (
 	"context"
 
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
+	"github.com/go-logr/logr"
 	djv1 "github.com/xvjixiang/DeamonJob/api/v1"
 )
 
 // DaemonJobReconciler reconciles a DaemonJob object
 type DaemonJobReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
 }
-
-//+kubebuilder:rbac:groups=dj.dysproz.io,resources=daemonjobs,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=dj.dysproz.io,resources=daemonjobs/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=dj.dysproz.io,resources=daemonjobs/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -46,17 +52,147 @@ type DaemonJobReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
-func (r *DaemonJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
+func (r *DaemonJobReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	// your logic here
+	_ = context.Background()
+	_ = r.Log.WithValues("daemonjob", req.NamespacedName)
+	r.Log.Info("Reconciling DaemonJob", "request name", req.Name, "request namespace", req.Namespace)
+	instance := &djv1.DaemonJob{}
+	instanceType := "daemonjob"
+	ctx := context.TODO()
+
+	if err := r.Client.Get(ctx, req.NamespacedName, instance); err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
+
+	if !instance.GetDeletionTimestamp().IsZero() {
+		return reconcile.Result{}, nil
+	}
+
+	nodeSelector := map[string]string{}
+	if instance.Spec.Template.Spec.NodeSelector != nil {
+		nodeSelector = instance.Spec.Template.Spec.NodeSelector
+	}
+	var nodes corev1.NodeList
+	if err := r.Client.List(ctx, &nodes, client.MatchingLabels(nodeSelector)); err != nil && errors.IsNotFound(err) {
+		return reconcile.Result{}, nil
+	}
+	jobReplicas := int32(len(nodes.Items))
+	job := getJob(instance, &jobReplicas, req.Name, instanceType)
+	err := controllerutil.SetControllerReference(instance, job, r.Scheme)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	var clusterJob batchv1.Job
+	clusterJob.ObjectMeta = job.ObjectMeta
+	_, err = ctrl.CreateOrUpdate(ctx, r, &clusterJob, func() error {
+		modifyJob(job, &clusterJob)
+		return controllerutil.SetControllerReference(instance, &clusterJob, r.Scheme)
+	})
+	if err != nil {
+		if errors.IsInvalid(err) {
+			_ = r.Client.Delete(ctx, &batchv1.Job{ObjectMeta: metav1.ObjectMeta{Name: job.Name, Namespace: job.Namespace}}, client.PropagationPolicy("Background"))
+			return reconcile.Result{RequeueAfter: 5}, nil
+		}
+		return reconcile.Result{}, err
+	}
+
+	instance.Status = &clusterJob.Status
+
+	return ctrl.Result{}, r.Client.Status().Update(context.TODO(), instance)
 
 	return ctrl.Result{}, nil
 }
 
+func getJob(instance *djv1.DaemonJob, replicas *int32, reqName, instanceType string) *batchv1.Job {
+	var jobAffinity = corev1.Affinity{
+		PodAntiAffinity: &corev1.PodAntiAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{{
+				LabelSelector: &metav1.LabelSelector{
+					MatchExpressions: []metav1.LabelSelectorRequirement{{
+						Key:      instanceType,
+						Operator: "In",
+						Values:   []string{reqName},
+					}},
+				},
+				TopologyKey: "kubernetes.io/hostname",
+			}},
+		},
+	}
+
+	var podSpec = instance.Spec.Template
+	podSpec.Spec.Affinity = &jobAffinity
+
+	if podSpec.Spec.RestartPolicy == "Always" {
+		podSpec.Spec.RestartPolicy = "OnFailure"
+	}
+
+	return &batchv1.Job{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Job",
+			APIVersion: "batch/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name + "-job",
+			Namespace: instance.Namespace,
+			Labels:    instance.Labels,
+		},
+		Spec: batchv1.JobSpec{
+			Parallelism:             replicas,
+			Completions:             replicas,
+			Selector:                instance.Spec.Selector,
+			Template:                podSpec,
+			ManualSelector:          instance.Spec.ManualSelector,
+			TTLSecondsAfterFinished: instance.Spec.TTLSecondsAfterFinished,
+			BackoffLimit:            instance.Spec.BackoffLimit,
+			ActiveDeadlineSeconds:   instance.Spec.ActiveDeadlineSeconds,
+		},
+	}
+}
+
+func modifyJob(job, clusterJob *batchv1.Job) {
+	clusterJob.Spec = job.Spec
+	clusterJob.Annotations = job.Annotations
+}
+
+// +kubebuilder:rbac:groups=dj.dysproz.io,resources=daemonjobs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=dj.dysproz.io,resources=daemonjobs/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *DaemonJobReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
+	if err := ctrl.NewControllerManagedBy(mgr).
 		For(&djv1.DaemonJob{}).
-		Complete(r)
+		Complete(r); err != nil {
+		return err
+	}
+
+	if err := ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.Node{}).
+		Watches(&source.Kind{Type: &corev1.Node{}},
+			handler.EnqueueRequestsFromMapFunc(func(a client.Object) []reconcile.Request {
+				var djObjects djv1.DaemonJobList
+				_ = mgr.GetClient().List(context.TODO(), &djObjects)
+				var requests = []reconcile.Request{}
+				for _, djObject := range djObjects.Items {
+					requests = append(requests, reconcile.Request{
+						NamespacedName: types.NamespacedName{
+							Name:      djObject.Name,
+							Namespace: djObject.Namespace,
+						},
+					})
+				}
+				return requests
+			}),
+		).
+		Complete(r); err != nil {
+		return err
+	}
+
+	return nil
 }
